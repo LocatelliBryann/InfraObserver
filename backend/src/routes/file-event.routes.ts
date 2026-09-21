96c2a8610cfc8ffac57ae668d7b10994ebc7ef37
LocatelliBryann <bryannlocatelli@gmail.com>
2026-09-21 19:32:16 -0300

    feat(backend): add recent file events endpoint

--- backend/src/routes/file-event.routes.ts
+++ backend/src/routes/file-event.routes.ts
@@ -4,6 +4,10 @@ import { fileEventController } from "../config/dependencies";
 
 const fileEventRouter = Router();
 
+fileEventRouter.get("/", (req, res) => {
+  void fileEventController.findRecent(req, res);
+});
+
 fileEventRouter.post("/", (req, res) => {
   void fileEventController.create(req, res);
 });
